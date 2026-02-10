6c63065836d8391e5560e4abd08af6458ca545d8
bredacrew <bredacrew@gmail.com>
2026-02-10 09:22:37 +0100

    Create connessione.php

---
+++ connessione.php
@@ -0,0 +1,26 @@
+<?php
+  // FILE: connessione.php
+  $host = "localhost";
+  $username = "vmartucci"; // <--- INSERISCI QUI IL TUO NOME UTENTE ALTERVISTA = nomeprogetto
+  $password = "";            // <--- SU ALTERVISTA LASCIA VUOTO (o metti la pass del pannello se l'hai cambiata)
+  $database = "my_vmartucci"; // <--- SCRIVI my_ SEGUITO DAL TUO NOME UTENTE
+
+  // Creiamo la connessione
+  $conn = new mysqli($host, $username, $password, $database);
+
+  // Verifichiamo se ci sono errori
+  if ($conn->connect_error) {
+      die("Errore di connessione al database: " . $conn->connect_error);
+  } else{
+      echo "Connessione al database avvenuta con successo!"; //da eliminare. Per DEBUG
+  }
+
+  // Impostiamo la codifica caratteri corretta
+  $conn->set_charset("utf8");
+?>
+            }
+          ?>
+        </tr>
+      </table>
+  </body>
+</html>

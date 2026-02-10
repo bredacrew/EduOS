4c74290bb8f339b673c59084a954c7054183753e
bredacrew <bredacrew@gmail.com>
2026-02-10 09:57:34 +0100

    index home page

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+      require_once "connessione.php";
+
+      if ($conn->connect_error) {
+     	die("Errore di connessione al database: " . $conn->connect_error);
+  	  }else{
+      	echo "connesione riuscita al db tramite file connessione.php";
+      }
+      
+    ?>
+</body>
+</html>

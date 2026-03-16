df8c4f4851ff8b254658d50060496f697b1b5df5
LoreCirulli <cirullilorenzo34@gmail.com>
2026-03-16 10:49:31 +0100

    Aggiunta pagina per la connessione al database

--- database/connessione.php
+++ database/connessione.php
@@ -1,1 +1,17 @@
+<?php
+    $host = "localhost";
+    $username = "eduos";
+    $password = "";
+    $database = "my_eduos";
 
+    $conn = new mysqli($host, $username, $password, $database);
+
+
+    if ($conn->connect_error) {
+        die("Errore di connessione al database: " . $conn->connect_error);
+    } else{
+        echo "Connessione al database avvenuta con successo!";
+    }
+
+    $conn->set_charset("utf8");
+?>
